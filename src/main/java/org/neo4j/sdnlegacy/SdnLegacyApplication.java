2334690948982a2b8f7388d090e482a002558a5d
Gerrit Meier <meistermeier@gmail.com>
2020-04-23 18:19:58 +0200

    Initial project setup

---
+++ src/main/java/org/neo4j/sdnlegacy/SdnLegacyApplication.java
@@ -0,0 +1,13 @@
+package org.neo4j.sdnlegacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdnLegacyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdnLegacyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/neo4j/sdnlegacy/SdnLegacyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.neo4j.sdnlegacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdnLegacyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
