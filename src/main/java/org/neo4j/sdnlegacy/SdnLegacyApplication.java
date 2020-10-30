a657c66e2a3ce20bfd0e7f3e1133b9eefbdac11b
Jennifer Reif <jennifer.reif@neo4j.com>
2020-10-30 11:23:33 -0500

    Fix errors on missing target node for relationship

--- src/main/java/org/neo4j/sdnlegacy/SdnLegacyApplication.java
+++ src/main/java/org/neo4j/sdnlegacy/SdnLegacyApplication.java
@@ -1,6 +1,5 @@
 package org.neo4j.sdnlegacy;
 
-import org.assertj.core.util.Lists;
 import org.neo4j.sdnlegacy.movie.Actor;
 import org.neo4j.sdnlegacy.movie.Tagline;
 import org.springframework.boot.SpringApplication;
@@ -9,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.core.convert.converter.GenericConverter;
 import org.springframework.data.neo4j.core.convert.Neo4jConversions;
 
+import java.util.Arrays;
 import java.util.List;
 
 @SpringBootApplication
@@ -20,8 +20,7 @@ public class SdnLegacyApplication {
 
 	@Bean
 	public Neo4jConversions neo4jConversions() {
-		List<? extends GenericConverter> converters = Lists
-				.list(new Tagline.TaglineConverter(), new Actor.ActorConverter());
+		List<? extends GenericConverter> converters = Arrays.asList(new Tagline.TaglineConverter(), new Actor.ActorConverter());
 
 		return new Neo4jConversions(converters);
 	}
