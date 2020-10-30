2543e7afc7f92441ed3f49815bbd078951a9469f
Jennifer Reif <jennifer.reif@neo4j.com>
2020-10-30 12:15:23 -0500

    clean up code by removing transactional annotation

--- src/test/java/org/neo4j/sdnlegacy/SdnLegacyApplicationTests.java
+++ src/test/java/org/neo4j/sdnlegacy/SdnLegacyApplicationTests.java
@@ -34,7 +34,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @Testcontainers
 @DataNeo4jTest
-@Transactional(propagation = Propagation.NOT_SUPPORTED)
 class SdnLegacyApplicationTests {
 
 	@Autowired
