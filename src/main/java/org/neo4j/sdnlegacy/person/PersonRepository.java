24c16b6fa30282f093fd99dceec2ccb1245745a3
Jennifer Reif <jennifer.reif@neo4j.com>
2020-11-10 15:48:04 -0600

    Initial commit

--- src/main/java/org/neo4j/sdnlegacy/person/PersonRepository.java
+++ src/main/java/org/neo4j/sdnlegacy/person/PersonRepository.java
@@ -1,5 +1,6 @@
 package org.neo4j.sdnlegacy.person;
 
+import org.springframework.data.neo4j.repository.query.Query;
 import reactor.core.publisher.Flux;
 
 import org.springframework.data.neo4j.repository.ReactiveNeo4jRepository;
@@ -12,4 +13,7 @@ public interface PersonRepository extends ReactiveNeo4jRepository<Person, Long> 
 	Flux<Person> findByReviewedMoviesMovieEntityTitle(String reviewedMovieTitle);
 
 	Flux<Person> findByDirectedMoviesTitle(String directedMovieTitle);
+
+	@Query("MATCH (m:Movie {title: $title})-[r:ACTED_IN]-(p:Person) RETURN m, r, p")
+	Flux<Person> findByActedInMovieTitle(String title);
 }
