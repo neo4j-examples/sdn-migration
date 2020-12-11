dbaf16de8e36a47b0bd950b53aa4b9a5804aabfa
Florent Biville <florent.biville@neo4j.com>
2020-12-11 17:59:52 +0100

    Show example of query for which semantics have changed since SDN6
    Fixes #32

--- src/main/java/org/neo4j/sdnlegacy/person/PersonRepository.java
+++ src/main/java/org/neo4j/sdnlegacy/person/PersonRepository.java
@@ -10,10 +10,13 @@ import java.util.List;
  */
 public interface PersonRepository extends Neo4jRepository<Person, Long> {
 
-	//List<Person> findByReviewedMoviesMovieNodeTitle(String reviewedMovieTitle);
+    //List<Person> findByReviewedMoviesMovieNodeTitle(String reviewedMovieTitle);
 
-	List<Person> findByDirectedMoviesTitle(String directedMovieTitle);
+    List<Person> findByDirectedMoviesTitle(String directedMovieTitle);
 
-	@Query("MATCH (m:Movie {title: $title})-[r:ACTED_IN]-(p:Person) RETURN m, r, p ORDER BY p.name")
-	List<ActedInMovieProjection> findByActedInMovieTitle(String title);
+    @Query("MATCH (m:Movie {title: $title})-[r:ACTED_IN]-(p:Person) RETURN m, r, p ORDER BY p.name")
+    List<ActedInMovieProjection> findByActedInMovieTitle(String title);
+
+    @Query("MATCH (p:Person{name:'Tom Hanks'})-[r:ACTED_IN]->(m:Movie) return p,r,m")
+    List<Person> tomHanksCareer();
 }
