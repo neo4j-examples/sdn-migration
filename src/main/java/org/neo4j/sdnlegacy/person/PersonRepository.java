0c3862b9ea666444e5d39da5ef44cfbfe1a37688
Florent Biville <florent.biville@neo4j.com>
2020-12-18 12:56:58 +0100

    Add query migration hint

--- src/main/java/org/neo4j/sdnlegacy/person/PersonRepository.java
+++ src/main/java/org/neo4j/sdnlegacy/person/PersonRepository.java
@@ -17,6 +17,9 @@ public interface PersonRepository extends Neo4jRepository<Person, Long> {
     @Query("MATCH (m:Movie {title: $title})-[r:ACTED_IN]-(p:Person) RETURN m, r, p ORDER BY p.name")
     List<ActedInMovieProjection> findByActedInMovieTitle(String title);
 
+    // see the related test to understand the semantic difference between SDN 5/OGM and SDN 6
+    // to keep SDN 5/OGM semantics, the query could become:
+    // MATCH (p:Person{name:'Tom Hanks'})-[r:ACTED_IN]->(m:Movie) return p,COLLECT(r),COLLECT(m)
     @Query("MATCH (p:Person{name:'Tom Hanks'})-[r:ACTED_IN]->(m:Movie) return p,r,m")
     List<Person> tomHanksCareer();
 }
