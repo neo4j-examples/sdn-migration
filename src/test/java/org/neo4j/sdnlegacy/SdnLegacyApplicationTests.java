7283ba15c16f22bd75def04c020037fad8b952b3
Florent Biville <florent.biville@neo4j.com>
2020-12-18 12:23:44 +0100

    Bring back nested tests
    Fixes #12

--- src/test/java/org/neo4j/sdnlegacy/SdnLegacyApplicationTests.java
+++ src/test/java/org/neo4j/sdnlegacy/SdnLegacyApplicationTests.java
@@ -2,6 +2,8 @@ package org.neo4j.sdnlegacy;
 
 import org.assertj.core.groups.Tuple;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.neo4j.driver.Driver;
 import org.neo4j.driver.Session;
@@ -17,6 +19,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.neo4j.core.ReactiveDatabaseSelectionProvider;
 import org.springframework.data.neo4j.core.ReactiveNeo4jTemplate;
 import org.springframework.data.neo4j.core.transaction.ReactiveNeo4jTransactionManager;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.transaction.annotation.Propagation;
@@ -41,19 +44,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Testcontainers
 @DataNeo4jTest
 @Transactional(propagation = Propagation.NOT_SUPPORTED)
+@ContextConfiguration(classes = SdnLegacyApplicationTests.Configuration.class)
 class SdnLegacyApplicationTests {
 
 	@Autowired
 	private Driver driver;
-
-	@Autowired
-	private ReactiveNeo4jTemplate neo4jTemplate;
-
-	@Autowired
-	private MovieRepository movieRepository;
-
-	@Autowired
-	private PersonRepository personRepository;
 
 	@Container
 	private static final Neo4jContainer<?> neo4jContainer = new Neo4jContainer<>("neo4j:4.0");
@@ -80,136 +75,153 @@ class SdnLegacyApplicationTests {
 		}
 	}
 
-	@Test
-	void findsAllMovies() {
-		int expectedMovieCount = 38;
-		StepVerifier.create(movieRepository.findAll())
-				.expectNextCount(expectedMovieCount)
-				.verifyComplete();
+	@Nested
+	@DisplayName("Movie Repository")
+	class MovieRepositoryTests {
+
+		@Autowired
+		private MovieRepository movieRepository;
+
+		@Test
+		void findsAllMovies() {
+			int expectedMovieCount = 38;
+			StepVerifier.create(movieRepository.findAll())
+					.expectNextCount(expectedMovieCount)
+					.verifyComplete();
+		}
+
+		@Test
+		void findsMovieByTitle() {
+			StepVerifier.create(movieRepository.findByTitle("The Matrix")).assertNext(movieEntity -> {
+				assertThat(movieEntity.getYearOfRelease()).isEqualTo(1999);
+			}).verifyComplete();
+		}
+
+		@Test
+		void findMoviesByActorsCypherPlaceholder() {
+			StepVerifier.create(movieRepository.findMoviesByActorNameWithCypherPlaceholder("Emil Eifrem")).assertNext(movieEntity -> {
+				assertThat(movieEntity.getTitle()).isEqualTo("The Matrix");
+			}).verifyComplete();
+		}
+
+		@Test
+		void findMoviesByActorsSpElIndexPlaceholder() {
+			StepVerifier.create(movieRepository.findMoviesByActorNameWithCypherPlaceholder("Emil Eifrem")).assertNext(movieEntity -> {
+				assertThat(movieEntity.getTitle()).isEqualTo("The Matrix");
+			}).verifyComplete();
+		}
+
+		@Test
+		void findMoviesByActorsSpElIndexColonPlaceholder() {
+			StepVerifier.create(movieRepository.findMoviesByActorNameWithCypherPlaceholder("Emil Eifrem")).assertNext(movieEntity -> {
+				assertThat(movieEntity.getTitle()).isEqualTo("The Matrix");
+			}).verifyComplete();
+		}
+
+		@Test
+		void findMoviesByActorsSpElNamedPlaceholder() {
+			StepVerifier.create(movieRepository.findMoviesByActorNameWithCypherPlaceholder("Emil Eifrem")).assertNext(movieEntity -> {
+				assertThat(movieEntity.getTitle()).isEqualTo("The Matrix");
+			}).verifyComplete();
+		}
+
+		@Test
+		void findMoviesByActorsSpElSearchObjectPlaceholder() {
+			StepVerifier.create(movieRepository.findMoviesByActorNameWithCypherPlaceholder("Emil Eifrem")).assertNext(movieEntity -> {
+				assertThat(movieEntity.getTitle()).isEqualTo("The Matrix");
+			}).verifyComplete();
+		}
+
+		@Test
+		void persistMovie(@Autowired ReactiveNeo4jTemplate neo4jTemplate) {
+			MovieEntity entity = new MovieEntity("MyMovie", "best catchy tagline ever", 2020);
+
+			StepVerifier.create(
+					movieRepository.save(entity)
+							.then(neo4jTemplate.findById("MyMovie", MovieEntity.class)))
+					.expectNextCount(1)
+					.verifyComplete();
+		}
+
+		@Test
+		void deleteMovie() {
+			Mono<Tuple2<Long, Long>> countPublisher = movieRepository.count()
+					.flatMap(oldCount -> movieRepository.deleteById("The Matrix")
+							.then(movieRepository.count())
+							.map(newCount -> Tuples.of(oldCount, newCount)));
+
+			StepVerifier.create(countPublisher)
+					.assertNext(counts -> {
+						final Long oldCount = counts.getT1();
+						final Long newCount = counts.getT2();
+						assertThat(newCount).isEqualTo(oldCount - 1);
+					})
+					.verifyComplete();
+		}
 	}
 
-	@Test
-	void findsMovieByTitle() {
-		StepVerifier.create(movieRepository.findByTitle("The Matrix")).assertNext(movieEntity -> {
-			assertThat(movieEntity.getYearOfRelease()).isEqualTo(1999);
-		}).verifyComplete();
-	}
 
-	@Test
-	void findMoviesByActorsCypherPlaceholder() {
-		StepVerifier.create(movieRepository.findMoviesByActorNameWithCypherPlaceholder("Emil Eifrem")).assertNext(movieEntity -> {
-			assertThat(movieEntity.getTitle()).isEqualTo("The Matrix");
-		}).verifyComplete();
-	}
+	@Nested
+	@DisplayName("Person Repository")
+	class PersonRepositoryTests {
 
-	@Test
-	void findMoviesByActorsSpElIndexPlaceholder() {
-		StepVerifier.create(movieRepository.findMoviesByActorNameWithCypherPlaceholder("Emil Eifrem")).assertNext(movieEntity -> {
-			assertThat(movieEntity.getTitle()).isEqualTo("The Matrix");
-		}).verifyComplete();
-	}
+		@Autowired
+		private PersonRepository personRepository;
 
-	@Test
-	void findMoviesByActorsSpElIndexColonPlaceholder() {
-		StepVerifier.create(movieRepository.findMoviesByActorNameWithCypherPlaceholder("Emil Eifrem")).assertNext(movieEntity -> {
-			assertThat(movieEntity.getTitle()).isEqualTo("The Matrix");
-		}).verifyComplete();
-	}
+		@Test
+		void findPersonsWhoReviewedCertainMovie() {
+			StepVerifier.create(personRepository.findByReviewedMoviesMovieEntityTitle("The Da Vinci Code"))
+					.expectNextCount(2)
+					.verifyComplete();
+		}
 
-	@Test
-	void findMoviesByActorsSpElNamedPlaceholder() {
-		StepVerifier.create(movieRepository.findMoviesByActorNameWithCypherPlaceholder("Emil Eifrem")).assertNext(movieEntity -> {
-			assertThat(movieEntity.getTitle()).isEqualTo("The Matrix");
-		}).verifyComplete();
-	}
+		@Test
+		void findPersonsWhoDirectedCertainMovie() {
+			StepVerifier.create(personRepository.findByDirectedMoviesTitle("The Da Vinci Code")).assertNext(personEntity -> {
+				assertThat(personEntity.getName()).isEqualTo("Ron Howard");
+			}).verifyComplete();
+		}
 
-	@Test
-	void findMoviesByActorsSpElSearchObjectPlaceholder() {
-		StepVerifier.create(movieRepository.findMoviesByActorNameWithCypherPlaceholder("Emil Eifrem")).assertNext(movieEntity -> {
-			assertThat(movieEntity.getTitle()).isEqualTo("The Matrix");
-		}).verifyComplete();
-	}
+		@Test
+		void findPersonsWhoActedInCertainMovie() {
+			StepVerifier.create(personRepository.findByActedInMovieTitle("The Da Vinci Code").buffer(4))
+					.assertNext(results -> assertThat(results)
+							.extracting(ActedInMovieProjection::getBorn, ActedInMovieProjection::getName)
+							.containsExactly(
+									actorInMovie(1976, "Audrey Tautou"),
+									actorInMovie(1939, "Ian McKellen"),
+									actorInMovie(1971, "Paul Bettany"),
+									actorInMovie(1956, "Tom Hanks")
+							))
+					.verifyComplete();
+		}
 
-	@Test
-	void persistMovie() {
-		MovieEntity entity = new MovieEntity("MyMovie", "best catchy tagline ever", 2020);
+		@Test
+		void tomHanksCareer() {
+			int expectedCount = 12;
+			StepVerifier.create(personRepository.tomHanksCareer().buffer(expectedCount))
+					.assertNext(results -> {
+						assertThat(results)
+								.overridingErrorMessage("Expected Spring Data Neo4j 6 to return as many rows as distinct patterns")
+								.hasSize(expectedCount)
+								.extracting(Person::getName)
+								.containsOnly("Tom Hanks");
+						Set<Integer> hashCodes = collectIdentityHashCodes(results);
+						assertThat(hashCodes)
+								.overridingErrorMessage("Expected Spring Data Neo4j 6.0.2+ to deduplicate instances")
+								.hasSize(1)
+								.containsOnly(hashCodes.iterator().next());
+					})
+					.verifyComplete();
+		}
 
-		StepVerifier.create(
-			movieRepository.save(entity)
-			.then(neo4jTemplate.findById("MyMovie", MovieEntity.class)))
-			.expectNextCount(1)
-			.verifyComplete();
-	}
+		private Set<Integer> collectIdentityHashCodes(List<Person> results) {
+			return results.stream().map(System::identityHashCode).collect(Collectors.toSet());
+		}
 
-	@Test
-	void deleteMovie() {
-		Mono<Tuple2<Long, Long>> countPublisher = movieRepository.count()
-				.flatMap(oldCount -> movieRepository.deleteById("The Matrix")
-						.then(movieRepository.count())
-						.map(newCount -> Tuples.of(oldCount, newCount)));
-
-		StepVerifier.create(countPublisher)
-				.assertNext(counts -> {
-					final Long oldCount = counts.getT1();
-					final Long newCount = counts.getT2();
-					assertThat(newCount).isEqualTo(oldCount - 1);
-				})
-				.verifyComplete();
-	}
-
-	@Test
-	void findPersonsWhoReviewedCertainMovie() {
-		StepVerifier.create(personRepository.findByReviewedMoviesMovieEntityTitle("The Da Vinci Code"))
-			.expectNextCount(2)
-			.verifyComplete();
-	}
-
-	@Test
-	void findPersonsWhoDirectedCertainMovie() {
-		StepVerifier.create(personRepository.findByDirectedMoviesTitle("The Da Vinci Code")).assertNext(personEntity -> {
-			assertThat(personEntity.getName()).isEqualTo("Ron Howard");
-		}).verifyComplete();
-	}
-
-	@Test
-	void findPersonsWhoActedInCertainMovie() {
-		StepVerifier.create(personRepository.findByActedInMovieTitle("The Da Vinci Code").buffer(4))
-				.assertNext(results -> assertThat(results)
-						.extracting(ActedInMovieProjection::getBorn, ActedInMovieProjection::getName)
-						.containsExactly(
-								actorInMovie(1976, "Audrey Tautou"),
-								actorInMovie(1939, "Ian McKellen"),
-								actorInMovie(1971, "Paul Bettany"),
-								actorInMovie(1956, "Tom Hanks")
-						))
-				.verifyComplete();
-	}
-
-	@Test
-	void tomHanksCareer() {
-		int expectedCount = 12;
-		StepVerifier.create(personRepository.tomHanksCareer().buffer(expectedCount))
-				.assertNext(results -> {
-					assertThat(results)
-							.overridingErrorMessage("Expected Spring Data Neo4j 6 to return as many rows as distinct patterns")
-							.hasSize(expectedCount)
-							.extracting(Person::getName)
-							.containsOnly("Tom Hanks");
-					Set<Integer> hashCodes = collectIdentityHashCodes(results);
-					assertThat(hashCodes)
-							.overridingErrorMessage("Expected Spring Data Neo4j 6.0.2+ to deduplicate instances")
-							.hasSize(1)
-							.containsOnly(hashCodes.iterator().next());
-				})
-				.verifyComplete();
-	}
-
-	private Set<Integer> collectIdentityHashCodes(List<Person> results) {
-		return results.stream().map(System::identityHashCode).collect(Collectors.toSet());
-	}
-
-	private Tuple actorInMovie(int born, String name) {
-		return Tuple.tuple(born, name);
+		private Tuple actorInMovie(int born, String name) {
+			return Tuple.tuple(born, name);
+		}
 	}
 
 	@TestConfiguration(proxyBeanMethods = false)
